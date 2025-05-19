7e983e2c1a9f65e3c19552fd7d77c664b05b7d20
Ammar allosh <ammaralosh2001@example.com>
2025-05-19 15:45:09 +0300

    ammar

--- app/Models/Frequently_question.php
+++ app/Models/Frequently_question.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Frequently_question extends Model
 {
     use HasFactory;
-    protected $table ='Frequently_questions';
+    protected $table ='frequently_questions';
     protected $primaryKey ='id';
     public $timestamps = true;
     protected $fillable =[
